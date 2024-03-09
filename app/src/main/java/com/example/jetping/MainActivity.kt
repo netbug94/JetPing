abf39c90a4962c326ecd14aa206fa9ebf087847e
olracnai <bongohmo@gmail.com>
2024-03-09 13:11:20 -0800

    Upgraded viewModel to the "new way"

--- app/src/main/java/com/example/jetping/MainActivity.kt
+++ app/src/main/java/com/example/jetping/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.jetping.engine_logic.JetPingViewModel
 import com.example.jetping.ui.theme.JetPingTheme
 
@@ -17,10 +17,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = ViewModelProvider(this)[JetPingViewModel::class.java]
             JetPingTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    val viewModel: JetPingViewModel = viewModel()
                     ScreenHandler(viewModel)
                 }
             }
